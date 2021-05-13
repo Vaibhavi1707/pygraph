8934eaf4e755e600defe34e5be33532f18f0cc47
Mahi Monga <61409868+mahimonga@users.noreply.github.com>
2021-05-14 05:00:33 +0530

    added Bellman-ford code.
    added Bellman-ford code.
    still in progress.

---
+++ src/pygraph/Shortest_path_BellmanFord.py
@@ -0,0 +1,33 @@
+class Graph:
+    def __init__(self):
+        self.v = vertices #number of vertices in graph
+        self.g = [] #edges list
+    
+    def add_edge(self,s,d,w):
+        self.g.append([s,d,w])
+
+    
+    def bellman_ford(self,src):
+        #for i in range(number of edges): 
+        #    self.g.append([s, d, w])
+
+        distance = float("Inf") * self.v
+        distance[src] = 0
+        
+    def relax(self,distance) -> int:
+        for i in range(self.v -1):
+            for s,d,w in self.g:
+                if distance[s] != float("Inf") and distance[s] + w < distance[d]:
+                    distance[d] = distance[s] + w
+        return distance 
+
+    def check_neg_cycle(self,distance) -> None:
+        for s, d, w in self.graph:
+            if distance[s] != float("Inf") and distance[s] + w < distance[d]:
+                return
+
+    def print_output(self):
+      #############
+
+
+#in progress
