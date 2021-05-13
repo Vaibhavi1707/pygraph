9d7f5ae94508214c9a9f8e863760d34cf6a6add0
Vaibhavi1707 <vaibhavilokegaonkar@gmail.com>
2021-05-13 20:53:56 +0530

    Added basic structure of BreadthFirstTraversal and constructor

---
+++ src/pygraph/BreadthFirstTraversal.py
@@ -0,0 +1,14 @@
+def breadth_first_search(self):
+    pass
+
+def unweighted_shortest_paths(self):
+    pass
+
+def bfs_tree(self):
+    pass
+
+def is_bipartite(self):
+    pass
+
+def is_connected(self):
+    pass
---
+++ src/pygraph/__init__.py
@@ -0,0 +1,22 @@
+def make_adjlist(vertices, edges):
+    adjlist = dict(zip(vertices, [[] for vertex in vertices]))
+    for u, v in edges:
+        adjlist[u].append(v)
+    return adjlist
+
+def make_adjmatrix(vertices, edges, weights):
+    adjmatrix = [[0 for vertex in vertices] for vertex in vertices]
+    for u, v in edges:
+        adjmatrix[u][v] = weights[u][v]
+    return adjmatrix
+
+class Graph():
+    def __init__(self, vertices, edges, weights = None):
+        self.v = vertices
+        self.e = edges
+        self.weights = weights
+        self.adjlist = make_adjlist(vertices, edges, weights)
+        self.adjmatrix = make_adjmatrix(edges, weights)
+        
+    from BreadthFirstTraversal import breadth_first_search, unweighted_shortest_paths, bfs_tree, is_bipartite, is_connected 
+        
