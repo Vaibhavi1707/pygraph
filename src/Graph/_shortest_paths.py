419503f20a83c070cdf92654d4ce4c35ffe300ac
Mahi Monga <61409868+mahimonga@users.noreply.github.com>
2021-05-15 01:36:29 +0530

    updated _shortest_paths.py

--- src/Graph/_shortest_paths.py
+++ src/Graph/_shortest_paths.py
@@ -11,7 +11,7 @@ def find_shortest_paths(self):
     Returns:
         List(List(int)): The n * n matrix delta, such that delta[i][j] = shortest distance between vertices i and j
     """
-    pass
+    
 
 def find_shortest_paths(self, s):
     """
@@ -40,37 +40,3 @@ def find_shortest_paths(self, s, t):
     """
     pass
 
-class Graph:
-    def __init__(self, vertices):
-        self.v = vertices #number of vertices in graph
-        self.g = [] #edges list
-    
-    def add_edge(self,s,d,w):
-        self.g.append([s,d,w])
-
-    
-    def bellman_ford(self,src):
-        #for i in range(number of edges): 
-        #    self.g.append([s, d, w])
-
-        distance = float("Inf") * self.v
-        distance[src] = 0
-        
-    def relax(self,distance) -> int:
-        for i in range(self.v -1):
-            for s,d,w in self.g:
-                if distance[s] != float("Inf") and distance[s] + w < distance[d]:
-                    distance[d] = distance[s] + w
-        return distance 
-
-    def check_neg_cycle(self,distance) -> None:
-        for s, d, w in self.graph:
-            if distance[s] != float("Inf") and distance[s] + w < distance[d]:
-                return
-
-    def print_output(self):
-      #############
-        pass        
-
-
-#in progress
