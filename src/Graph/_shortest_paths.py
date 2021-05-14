10c76ebfd497d5320fe10a09f6741cafa7f6b74c
Mahi Monga <61409868+mahimonga@users.noreply.github.com>
2021-05-15 03:51:34 +0530

    updated _shortest_paths.py with bellman-ford code.

--- src/Graph/_shortest_paths.py
+++ src/Graph/_shortest_paths.py
@@ -3,17 +3,18 @@ import math, sys
 # sys.path.insert(1, "../../../")
 # import pygraph.src.Graph
 
-# Complete the functions below
 
-def find_shortest_paths(self):
+
+def find_shortest_paths(self,):
     """
     Finds the shortest distance in all pairs of vertices in a weighted graph.
     Returns:
         List(List(int)): The n * n matrix delta, such that delta[i][j] = shortest distance between vertices i and j
     """
     
+    
 
-def find_shortest_paths(self, s):
+def find_shortest_paths(self,graph,s):
     """
     Returns the shortest distance from a source s to all other vertices 
         in a weighted graph.
@@ -26,7 +27,25 @@ def find_shortest_paths(self, s):
         delta (List(int)): Shortest path lengths between source s to all other 
                 vertices of a weighted graph. 
     """
-    pass
+    #intialize distance and predecessor
+    dist, pre = dict(), dict()
+    for vertex in graph:
+        dist[vertex], pre[vertex] = float('inf'), None
+    dist[s] = 0 
+
+    #Relax the edges
+    for i in range(len(graph) - 1):
+        for vertex in graph:
+            for adj_vertex in graph[vertex]:
+                if dist[adj_vertex] > dist[vertex] + graph[vertex][adj_vertex]:
+                    dist[adj_vertex], pre[adj_vertex] = dist[vertex] + graph[vertex][adj_vertex], vertex
+
+    #check for negative cycles
+    for vertex in graph:
+        for adj_vertex in graph[vertex]:
+            assert dist[adj_vertex] <= dist[vertex] + graph[vertex][adj_vertex], "Negative weight cycle."
+ 
+    return dist, pre
 
 def find_shortest_paths(self, s, t):
     """
